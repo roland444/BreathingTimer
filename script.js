f40327ef941e1fbc9b43ea4cd5c0e64f45ab32c0
roland444 <123905024+roland444@users.noreply.github.com>
2024-03-19 22:53:38 +0100

    Changed type of sessions settings

--- script.js
+++ script.js
@@ -1,40 +1,123 @@
 //const _inputs = document.querySelectorAll("input");
-const _startBtn = document.querySelector("#start-btn");
-const _timer = document.querySelector(".timer");
+// const _startBtn = document.querySelector("#start-btn");
+// const _timer = document.querySelector(".timer");
 
-let sessions = document.querySelector("#sessions").value;
-let seconds = document.querySelector("#seconds").value;
+// let sessions = document.querySelector("#sessions").value;
+// let seconds = document.querySelector("#seconds").value;
 
-[sessions, seconds] = [parseInt(sessions), parseInt(seconds)];
+// sessions settings
 
-let holding = 15;
+const addButton = document.querySelector(".add-session");
+const rmvButton = document.querySelector(".remove-session");
+const sessionsAmount = document.querySelector("#session-value");
 
-function initBreathingTimer() {
-    let amountOfSessions = sessions;
-    let amountOfSeconds = seconds;
-    let amountOfHolding = holding;
 
-    function startTimer() {
-        const timer = setInterval(() => {
-            _timer.innerHTML = `${amountOfSeconds}`;
-            amountOfSeconds--;
+window.onload = () => {
+    sessionsAmount.innerHTML = 3;
 
-            if (amountOfSeconds < -1) {
-                _timer.innerHTML = `${amountOfHolding}`;
-                amountOfHolding--;
-                
-                if (amountOfHolding < 0) {
-                    [amountOfSeconds, amountOfHolding] = [seconds, holding]
-                    amountOfSessions--;
-                
-                    if (amountOfSessions === 0) {
-                        clearInterval(timer);
-                    }       
-                }
-            }
-        }, 1000)  
-    }
-    startTimer();
 }
 
-_startBtn.addEventListener("click", initBreathingTimer);
+function addSession() {
+    if (sessionsAmount.innerHTML === "10") {
+        addButton.diabled = true;
+
+        rmvButton.disabled = false;
+    } else {
+        sessionsAmount.innerHTML++;
+    }
+}
+
+function rmvSession() {
+    if (sessionsAmount.innerHTML === "1") {
+        rmvButton.disabled = true;
+
+        addButton.disabled = false;
+    } else {
+        sessionsAmount.innerHTML--;
+    }
+    
+}
+
+
+
+addButton.addEventListener("click", addSession);
+
+rmvButton.addEventListener("click", rmvSession);
+
+
+//if (sessionsAmount.innerText)
+
+
+// Event Listeners
+
+
+
+//rmvButton.addEventListener("click", rmvSession);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//[sessions, seconds] = [parseInt(sessions), parseInt(seconds)];
+
+// let holding = 15;
+
+// function initBreathingTimer() {
+//     let amountOfSessions = sessions;
+//     let amountOfSeconds = seconds;
+//     let amountOfHolding = holding;
+
+//     function startTimer() {
+//         const timer = setInterval(() => {
+//             _timer.innerHTML = `${amountOfSeconds}`;
+//             amountOfSeconds--;
+
+//             if (amountOfSeconds < -1) {
+//                 _timer.innerHTML = `${amountOfHolding}`;
+//                 amountOfHolding--;
+                
+//                 if (amountOfHolding < 0) {
+//                     [amountOfSeconds, amountOfHolding] = [seconds, holding]
+//                     amountOfSessions--;
+                
+//                     if (amountOfSessions === 0) {
+//                         clearInterval(timer);
+//                     }       
+//                 }
+//             }
+//         }, 1000)  
+//     }
+//     startTimer();
+// }
+
+//_startBtn.addEventListener("click", initBreathingTimer);
